3e39bd3ab8fb182185ead126ee1f7ed33eaa73b7
João Vitor Chaves <joaovmanciochaves@gmail.com>
2026-09-13 16:24:18 -0300

    fix: correção no package.json de apps/backend e export do app em express

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,4 +2,8 @@ import { app } from './server';
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Server running on: \n> http://localhost:${PORT}\n`));
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => console.log(`Server running on: \n> http://localhost:${PORT}\n`))
+}
+
+export default app;
