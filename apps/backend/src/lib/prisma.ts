e859e2e218677060199735440253cc4d67a46107
luizadugois <luiza.dugois@unifesp.br>
2026-09-15 21:28:00 -0300

    aprendendo git

--- apps/backend/src/lib/prisma.ts
+++ apps/backend/src/lib/prisma.ts
@@ -3,7 +3,7 @@ import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
 
 config({ path: '../../.env' });
-
+#qualquer coisa
 const globalForPrisma = globalThis as typeof globalThis & { prisma?: PrismaClient };
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL ?? '' });
 
