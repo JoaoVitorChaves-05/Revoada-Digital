c067149f04d1e84db2967c0cec0f4c853217c8e7
Guilherme Ferro <guilhermeferro@MacBook-Air-de-Guilherme.local>
2026-09-24 02:23:42 -0300

    feat(questions): adiciona rotas C/R no schema

--- apps/backend/src/schemas/question.schema.ts
+++ apps/backend/src/schemas/question.schema.ts
@@ -3,10 +3,18 @@ import { z } from 'zod';
 const questionFieldsSchema = z.object({
     text: z.string().trim().min(1, 'O enunciado da questão é obrigatório'),
     difficulty: z.number().int('A dificuldade deve ser um número inteiro').min(1, 'A dificuldade mínima é 1'),
+    alternatives: z.array(
+        z.object({
+            text: z.string().trim().min(1, 'O texto da alternativa é obrigatório'),
+            isCorrect: z.boolean()
+        })
+    ).min(1, 'É obrigatório enviar pelo menos uma alternativa')
+     .refine((alts) => alts.some((alt) => alt.isCorrect === true), {
+         message: 'Pelo menos uma alternativa deve estar correta',
+     })
 });
 
 export const createQuestionSchema = questionFieldsSchema;
-
 export const updateQuestionSchema = questionFieldsSchema.partial();
 
 export type CreateQuestionInput = z.infer<typeof createQuestionSchema>;
